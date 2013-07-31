5b0aaf2452f306f6b7a671863511998d23f98a07
Tim Goodwin <trgoodwin@gmail.com>
2013-07-31 05:53:45 +0100

    reverting to latest released code

--- test/clj_soap/test/core.clj
+++ test/clj_soap/test/core.clj
@@ -25,6 +25,3 @@
     ;(is (= "abcabc" (cl :doubl2 "abc")))
     ))
 
-;;;; Test for exteral SOAP service
-(let [client (client-fn "http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL")]
-  (client :GetCityWeatherByZIP "16001"))
